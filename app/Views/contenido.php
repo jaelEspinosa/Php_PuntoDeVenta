262d8776772717097d7722d3b3475b1f11d7d167
jaelEspinosa <espinosa.lucia@bootcamp-upgrade.com>
2023-06-04 22:33:54 +0200

    layout

---
+++ app/Views/contenido.php
@@ -0,0 +1,9 @@
+
+
+
+<div id="layoutSidenav_content">
+    <main>
+        <div class="container-fluid px-4">
+            <h1>Hola vista del contenido</h1>
+        </div>
+    </main>
---
+++ app/Views/footer.php
@@ -0,0 +1,19 @@
+<footer class="py-4 bg-light mt-auto">
+                    <div class="container-fluid px-4">
+                        <div class="d-flex align-items-center justify-content-between small">
+                            <div class="text-muted">Copyright &copy; <?php echo date('Y')?></div>
+                            <div>
+                                <i class= "fa-brands fa-github"></i>
+                                <a href="https://github.com/jaelespinosa" target="_blank">GitHub jaelEspinosa</a>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+            </div>
+        </div>
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <script src="js/scripts.js"></script>
+        <script src="js/simple-datatables.min.js"></script>
+        <script src="js/datatables-simple-demo.js"></script>
+    </body>
+</html>
